87c6eebba5ff4c5deb2e0a573226eedf58cc9c26
Qamar Aziz <azizqamar7@gmail.com>
2024-10-10 18:44:02 +0530

    added footer marquee ix

--- src/helpers/marquee-scroll.js
+++ src/helpers/marquee-scroll.js
@@ -4,6 +4,14 @@ export const marqueeScrollIx = () => {
   const tl = gsap.timeline({ repeat: -1 })
 
   tl.to('[marquee-track]', { x: '-50%', ease: 'none', duration: 50 })
+
+  const footerTl = gsap.timeline({ repeat: -1 })
+
+  footerTl.to('[marquee-track-footer]', {
+    x: '-50%',
+    ease: 'none',
+    duration: 40,
+  })
 }
 
 export const marqueeVerticalScroll = () => {
